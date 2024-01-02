6babcb3905088becf3da243a519922bee4508252
wilsonwanfung <122155502+wilsonwanfung@users.noreply.github.com>
2024-01-02 20:44:54 +0800

    Update strat.py

--- strat.py
+++ strat.py
@@ -432,19 +432,10 @@ class AlgoEvent:
         
     # execute the trading strat for one instructment given the key and bd       
     def execute_strat(self, bd, key):
-        #self.evt.consoleLog("---------------------------------")
-        #self.evt.consoleLog("Executing strat")
-
-        # debug
-        #self.evt.consoleLog(f"name of instrument: { bd[key]['instrument'] }")
-        #self.evt.consoleLog(f"datetime: {bd[self.myinstrument]['timestamp']}")
-        #self.evt.consoleLog(f"upper: {upper_bband}")
-        #self.evt.consoleLog(f"lower: {lower_bband}")
-        #self.evt.consoleLog(f"bbw: {bbw}")
         
         inst = self.inst_data[key]
         lastprice =  inst['arr_close'][-1]
-        
+        position_size = self.allocate_capital( self.calculate_strategy_returns(inst['arr_close']) )
         # set direction, ie decide if buy or sell, based on entry signal
         direction = 1
         if inst['entry_signal'] > 0:
@@ -465,10 +456,22 @@ class AlgoEvent:
             # if current position exist in open order as well as opposite direction and same trading signal, close the order
             self.closeAllOrder(instrument, self.openOrder[instrument][orderRef])
             
-        self.test_sendOrder(lastprice, direction, 'open', stoploss, takeprofit, (self.no_of_trade_today/self.no_of_inst)* self.find_positionSize(lastprice), key, inst['entry_signal'] )
-                
-        #self.evt.consoleLog("Executed strat")
-        #self.evt.consoleLog("---------------------------------")
+        self.test_sendOrder(lastprice, direction, 'open', stoploss, takeprofit, position_size, key, inst['entry_signal'] )
+        
+    def calculate_strategy_returns(self, prices):
+        returns = []
+        for i in range(1, len(prices)):
+            daily_return = (prices[i] - prices[i-1]) / prices[i-1]
+            returns.append(daily_return)
+        return returns
+        
+    def allocate_capital(self, strategy_returns):
+        total_returns = sum(strategy_returns)
+        weights = strategy_returns[-1] / total_returns
+        res = self.evt.getAccountBalance()
+        bal = res["availableBalance"]
+        allocated_capital = weights*bal
+        return allocated_capital       
 
 
     def test_sendOrder(self, lastprice, buysell, openclose, stoploss, takeprofit, volume, instrument, orderRef):
@@ -547,3 +550,4 @@ class AlgoEvent:
             volume = (availableBalance*ratio) / lastprice
             total =  volume *  lastprice
         return volume
+    
